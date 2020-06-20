0c4fe3fc212decc9e1e3829a5c03984578017950
Paperless Warehousing Group <pwh_coderepo@paperlesswarehousing.com.au>
2020-06-20 16:24:00 +1000

    Initial commit

---
+++ argparse_tester.py
@@ -0,0 +1,32 @@
+
+
+
+
+
+#
+#
+#
+# #TEST
+# 1. more than 2 variables: DONE
+# 2. -r or -d is not passed: DONE
+# 3. number (or not a number) not passed along with -r
+# 4. emailId (or not a valid emailId) not passed along with -d
+# 5. less than 2 variables: DONE
+# 6.
+#
+
+
+
+
+#
+# my_parser = argparse.ArgumentParser()
+#
+# my_parser.add_argument('r', help='Read the Email', required=False)
+# my_parser.add_argument('d', help='Delete the Email', required=False)
+#
+#
+# # Execute parse_args()
+# args = my_parser.parse_args()
+#
+# print('If you read this line it means that you have provided '
+#       'all the parameters')
---
+++ socket_connection.py
@@ -0,0 +1,30 @@
+import socket  # for socket
+import sys
+
+ip = "127.0.0.1"
+port = 8230
+FIRST_MSG_BYTES = bytearray(r"configMsg,01-06-20 12:17:07,DSWA,1234,VIO1.3,en_AU\015\012".encode("utf-8"))
+# SECOND_MSG_BYTES = bytearray(r"signOn,01-06-20 12:30:03,DSWA,1234\015\012".encode("utf-8"))
+SECOND_MSG_BYTES = bytearray(r"signOn,01-06-20 12:30:03,DSWA,1234".encode("utf-8"))
+
+try:
+    s = socket.socket()
+    print("Socket successfully created")
+except socket.error as err:
+    print("socket creation failed with error %s" % err)
+
+# connecting to the server
+con = s.connect((ip, port))
+
+print("Successfully connected on {}:{}".format(ip, port))
+try:
+    response = s.send(FIRST_MSG_BYTES)
+    print("First Response:", response)
+
+    # response = s.send(SECOND_MSG_BYTES)
+    # print("Second Response:", response)
+    # print(s.recv(10))
+finally:
+    # close the connection
+    s.close()
+    print("DONE")
