98295817521a12c37fd481790d7fe3d0bb57fcae
Paperless Warehousing Group <pwh_coderepo@paperlesswarehousing.com.au>
2020-06-20 16:24:45 +1000

    messy

--- socket_connection.py
+++ socket_connection.py
@@ -1,6 +1,8 @@
 import socket  # for socket
 import sys
 
+messy
+
 ip = "127.0.0.1"
 port = 8230
 FIRST_MSG_BYTES = bytearray(r"configMsg,01-06-20 12:17:07,DSWA,1234,VIO1.3,en_AU\015\012".encode("utf-8"))
